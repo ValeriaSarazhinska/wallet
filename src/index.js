f45f5bc057c652873db98f8a2a3480c6b4ea3224
ValeriaSarazhinska <lera.sarazhinska@gmail.com>
2023-03-14 13:14:05 +0200

    Refactored BrowserRouter

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <PersistGate loading={null} persistor={persistor}>
       <Provider store={store}>
         <BrowserRouter
-        // basename="Wallet"
+       basename="Wallet"
         >
           <App />
         </BrowserRouter>
